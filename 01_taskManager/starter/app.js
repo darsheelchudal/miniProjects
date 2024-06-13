2a67d0aa0528aba24327a98947599e59ad54163c
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-13 06:39:43 +0545

    env config

--- 01_taskManager/starter/app.js
+++ 01_taskManager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+import express from "express";
+
+const app = express();
+
+//Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Task Manager App");
+});
+
+//  ("/api/v1/tasks") -get all the tasks
+//  ("/api/v1/tasks") -create a new tasks
+//  ("/api/v1/tasks/:id") -get single tasks
+//  ("/api/v1/tasks/:id") -update task
+//  ("/api/v1/tasks/:id") -delete task
+
+const PORT = 8000;
+app.listen(PORT, console.log(`Server is listening at Port ${PORT}...`));
