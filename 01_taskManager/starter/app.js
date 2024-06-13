c073c4379f902e96848e2d9336dc42db51d0cac4
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-13 07:11:34 +0545

    route setup

--- 01_taskManager/starter/app.js
+++ 01_taskManager/starter/app.js
@@ -1,17 +1,23 @@
 import express from "express";
+import tasks from "./routes/tasks.js";
 
 const app = express();
+
+//Middlewares
+app.use(express.json());
 
 //Routes
 app.get("/", (req, res) => {
   res.status(200).send("Task Manager App");
 });
 
-//  ("/api/v1/tasks") -get all the tasks
-//  ("/api/v1/tasks") -create a new tasks
-//  ("/api/v1/tasks/:id") -get single tasks
-//  ("/api/v1/tasks/:id") -update task
-//  ("/api/v1/tasks/:id") -delete task
+app.use("/api/v1/tasks", tasks);
+
+//  get("/api/v1/tasks") -get all the tasks
+//  post("/api/v1/tasks") -create a new tasks
+//  get("/api/v1/tasks/:id") -get single task
+//  patch("/api/v1/tasks/:id") -update task
+//  delete("/api/v1/tasks/:id") -delete task
 
 const PORT = 8000;
 app.listen(PORT, console.log(`Server is listening at Port ${PORT}...`));
---
+++ 01_taskManager/starter/controllers/tasks.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const getAllTasks = (req, res) => {
+  res.send("Get all tasks");
+};
+
+const createTask = (req, res) => {
+  res.send("Create tasks");
+};
+
+const getTask = (req, res) => {
+  res.send("Get single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+export { getAllTasks, createTask, getTask, updateTask, deleteTask };
