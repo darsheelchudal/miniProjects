497973e9ac4cfb063313d92083a7dafe368595de
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-17 20:15:19 +0545

    updateTask

--- 01_taskManager/starter/controllers/tasks.js
+++ 01_taskManager/starter/controllers/tasks.js
@@ -33,8 +33,17 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
