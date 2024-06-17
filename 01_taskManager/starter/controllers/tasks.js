70ae5b9c9b510efde410b6acc039c01a6eea2fae
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-17 21:21:25 +0545

    update rest operation

--- 01_taskManager/starter/controllers/tasks.js
+++ 01_taskManager/starter/controllers/tasks.js
@@ -33,19 +33,6 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID });
-    if (!task) {
-      return res.status(404).json({ message: `No task with id : ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-};
-
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
@@ -58,6 +45,24 @@ const deleteTask = async (req, res) => {
     res.status(200).json({ task });
   } catch (error) {
     res.send(500).json({ message: error });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      res.status(404).json({
+        message: `No task with id : ${taskID} that can be deleted !!!`,
+      });
+    }
+    res.status(200).json({ id: taskID, data: req.body });
+  } catch (error) {
+    res.status(500).json({ id: error });
   }
 };
 
