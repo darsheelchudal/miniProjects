4c0ed83f407b390cf8b606e91919aa70726476dd
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-18 05:26:44 +0545

    task handling

--- 01_taskManager/starter/app.js
+++ 01_taskManager/starter/app.js
@@ -9,11 +9,7 @@ const app = express();
 
 //Middlewares
 app.use(express.json());
-
-//Routes
-app.get("/", (req, res) => {
-  res.status(200).send("Task Manager App");
-});
+app.use(express.static("./public"));
 
 app.use("/api/v1/tasks", tasks);
 
@@ -21,7 +17,7 @@ app.use("/api/v1/tasks", tasks);
 //  post("/api/v1/tasks") -create a new tasks
 //  get("/api/v1/tasks/:id") -get single task
 //  patch("/api/v1/tasks/:id") -update task
-//  delete("/api/v1/tasks/:id") -delete task
+//   delete("/api/v1/tasks/:id") -delete task
 
 const PORT = 8000;
 
